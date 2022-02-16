31ab34e80108554358d2e897d3150892f07757c1
Xavier Naudé <xnaude@svp.com>
2022-02-16 11:42:47 +0100

    update header

---
+++ assets/js/app_header.js
@@ -0,0 +1,28 @@
+'use strict;'
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+    let btn_burger = document.querySelector('#burger-toggle');
+
+    btn_burger.addEventListener('change', function () {
+
+        if (btn_burger.checked) {
+            document.querySelector('body').style.overflow = 'hidden';
+        } else {
+            document.querySelector('body').style.overflow = '';
+        }
+    })
+
+    window.addEventListener('scroll', function () {
+
+        if(window.pageYOffset) {
+            document.querySelector('.nav-item-2').classList.add('d-none')
+            document.querySelector('.div-logo').maxHeight = "5vh";
+
+        }else{
+            document.querySelector('.nav-item-2').classList.remove('d-none')
+            document.querySelector('.div-logo').maxHeight = "10vh";
+        }
+    })
+
+});
