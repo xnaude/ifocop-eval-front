2eabc714fec64848d570c2e4abcb3be252c53430
Xavier Naudé <xnaude@svp.com>
2022-02-19 22:16:50 +0100

    update js

--- assets/js/app_header.js
+++ assets/js/app_header.js
@@ -4,25 +4,29 @@ window.addEventListener('DOMContentLoaded', () => {
     
     let btn_burger = document.querySelector('#burger-toggle');
 
-    btn_burger.addEventListener('change', function () {
+    if(window.innerWidth < 991) {
+        btn_burger.addEventListener('change', function () {
 
-        if (btn_burger.checked) {
-            document.querySelector('body').style.overflow = 'hidden';
-        } else {
-            document.querySelector('body').style.overflow = '';
-        }
-    });
-
-    window.addEventListener('scroll', function () {
-
-        if(window.pageYOffset) {
-            document.querySelector('.nav-item-2').classList.add('d-none')
-            document.querySelector('.div-logo').maxHeight = "5vh";
-
-        }else{
-            document.querySelector('.nav-item-2').classList.remove('d-none')
-            document.querySelector('.div-logo').maxHeight = "10vh";
-        }
-    });
+            if (btn_burger.checked) {
+                document.querySelector('body').style.overflow = 'hidden';
+            } else {
+                document.querySelector('body').style.overflow = '';
+            }
+        });
+    }
+    
+    if(window.innerWidth > 991) {
+        window.addEventListener('scroll', function () {
+    
+            if(window.pageYOffset) {
+                document.querySelector('.nav-item-2').classList.add('d-none')
+                document.querySelector('.div-logo').maxHeight = "5vh";
+    
+            }else{
+                document.querySelector('.nav-item-2').classList.remove('d-none')
+                document.querySelector('.div-logo').maxHeight = "10vh";
+            }
+        });
+    }
 
 });
